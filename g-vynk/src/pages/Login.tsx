834de2eb94343a43b71b98e8bb4e26a0a9271ad5
GUSELTONY <devguselt@gmail.com>
2026-09-14 18:53:47 +0100

    create basic react app with page routing

---
+++ g-vynk/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Mode = "login" | "register";
+
+const Login = ({ mode }: { mode: Mode }) => {
+  // {
+  //   mode === "login" ? "Login" : "Register";
+  // }
+  return <div>login</div>;
+};
+
+export default Login;
