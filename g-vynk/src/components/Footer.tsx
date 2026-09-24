f34acf5e7c8bf6c9aee4fad6102edac72f66f03c
GUSELTONY <devguselt@gmail.com>
2026-09-24 16:09:40 +0100

    created pages and route with navbar

--- g-vynk/src/components/Footer.tsx
+++ g-vynk/src/components/Footer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-full max-w-305 max-auto bg-white/10 backdrop-blur xl:rounded-t-xl px-6 py-6 text-center">
+      <p className="text-xs font-medium text-white">
+        © 2026 G-Vynk. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- g-vynk/src/pages/Login.tsx
+++ g-vynk/src/pages/Login.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import { SignIn, SignUp, useUser } from "@clerk/react";
+import { Navigate } from "react-router-dom";
 
 type Mode = "login" | "register";
 
 const Login = ({ mode }: { mode: Mode }) => {
-  // {
-  //   mode === "login" ? "Login" : "Register";
-  // }
-  return <div>login</div>;
+  const isRegister = mode === "register";
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (isLoaded && isSignedIn) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return (
+    <div className="min-h-screen w-full bg-[url('/login_bg.png')] text-slate-800 p-4 md:p-6 lg:p-8 flex items-center justify-center font-sans">
+      <div className="w-full flex justify-center py-2">
+        {isRegister ? (
+          <SignUp
+            routing="path"
+            path="/register"
+            signInUrl="/login"
+            fallbackRedirectUrl="/dashboard"
+          />
+        ) : (
+          <SignIn
+            routing="path"
+            path="/login"
+            signInUrl="/register"
+            fallbackRedirectUrl="/dashboard"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
